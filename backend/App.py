9056f74e9b26a3ee53f0ccd834111a54b7c47b4e
lrangu2 <lrangu2@illinois.edu>
2025-03-01 16:12:24 -0600

    api push

--- backend/App.py
+++ backend/App.py
@@ -1,1 +1,19 @@
 # starting
+import random
+
+def get_random_tickers(num_tickers=5):
+    exchanges = ['NYSE', 'NASDAQ', 'AMEX']
+    characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    
+    random_tickers = []
+    
+    for _ in range(num_tickers):
+        ticker_length = random.randint(1, 5)
+        ticker = ''.join(random.choice(characters) for _ in range(ticker_length))
+        exchange = random.choice(exchanges)
+        random_tickers.append(f"{ticker}:{exchange}")
+    
+    return random_tickers
+
+random_tickers = get_random_tickers()
+print("Random Tickers:", random_tickers)
